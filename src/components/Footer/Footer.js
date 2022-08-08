26a326686b58d5d7c108f730646757ba3c79b6df
손소희 <ssh30510044@gmail.com>
2022-08-08 17:02:57 +0900

    Add: header, footer 기본적인 ui 구현

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,76 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <FooterBox>
+        <h2>뉴스레이터에 가입하세요</h2>
+        <ul>
+          <li>TIKTOK</li>
+          <li>INSTAGRAM</li>
+          <li>FACEBOOK</li>
+          <li>TWITTER</li>
+          <li>PINTEREST</li>
+          <li>KAKAO</li>
+          <li>YOUTUBE</li>
+          <li>SPOTIFY</li>
+        </ul>
+      </FooterBox>
+      <CompanyInfo>
+        아이티엑스코리아 유한회사 ｜ 사업자등록번호: 120-88-14733 ｜ 대표자 :
+        ROMAY DE LA COLINA JOSE MANUEL ｜ 서울시 강남구 영동대로 511 (삼성동,
+        트레이드타워 33층) ｜ 대표번호: 080-479-0880 | 이메일:
+        contact.kr@zara.com ｜ 호스팅 서비스 사업자: ITX Merken, B.V. ｜
+        통신판매업신고 : 제2014-서울강남-02297 (사업자정보확인) |
+        개인정보처리방침 | 이용약관
+      </CompanyInfo>
+    </Container>
+  );
 }
+const Container = styled.div`
+  /* margin: 0 auto; */
+  margin: 0 70px;
+`;
+const FooterBox = styled.div`
+  flex-grow: 1;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  h2 {
+    margin-top: 200px;
+    font-size: 20px;
+    line-height: 24px;
+    letter-spacing: 3px;
+    font-weight: lighter;
+  }
+  ul {
+    margin-bottom: 200px;
+    margin-top: 100px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding: 0;
+    margin-top: 100px;
+    font-size: 10px;
+    line-height: 14px;
+    li {
+      margin: 8px;
+      letter-spacing: 0.8px;
+      transform: scaleY(1.3);
+    }
+  }
+`;
+const CompanyInfo = styled.div`
+  margin: 12px 0 56px 56.5px;
+  padding: 8px 0;
+  width: 40%;
+  font-size: 9px;
+  line-height: 16px;
+  letter-spacing: 0.8px;
+  text-transform: uppercase;
+`;
 
 export default Footer;
